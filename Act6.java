ddb48ca661c43788eb9fa5b68b6f18bf40f6a0ca
MaferMier08 <80357831+MaferMier08@users.noreply.github.com>
2021-04-06 22:37:49 -0500

    Add files via upload

---
+++ Act6.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Act6 {
+
+    public static void main(String[] args) {
+        // Pedir nombre
+        Scanner name = new Scanner(System.in);
+        System.out.printf("Ingrese el nombre del estudiante: ");
+        String student = name.nextLine();
+
+        // Pedir las 5 calificaciones
+        int[] grades = new int[5];
+        Scanner grade = new Scanner(System.in);
+        for (int i = 0; i < grades.length; i++){
+            System.out.printf("Calificación " + (i + 1)+ ": ");
+            grades[i] = grade.nextInt();
+        }
+        Imprimir(student, grades);
+    }
+
+    //Promedio
+    public static float Average (int grades[]){
+        int addition = 0;
+        for(int i=0; i< grades.length; i++){
+            addition += grades[i];
+        }
+        float average = addition/5;
+        return average;
+
+    }
+
+    // Promedio en letra
+    public static char Letter (int average){
+        char letter = 'Z';
+        if (average <= 50){
+            letter = 'F';
+        }else if (average >50 && average < 61){
+            letter = 'E';
+        }else if (average >60 && average < 71){
+            letter = 'D';
+        }else if (average > 70 && average < 81){
+            letter = 'C';
+        }else if (average >80 && average < 91){
+            letter = 'B';
+        }else if (average >90 && average < 101) {
+            letter = 'A';
+        }else {
+            letter = 'N';
+        }
+        return letter;
+    }
+
+    //Imprimir
+    public static void Imprimir  (String student, int grades[]){
+        System.out.println("Nombre del estudiante: " + student);
+        for (int i = 0; i < grades.length; i++){
+            System.out.println("Calificación " + (i + 1)+ ": "+grades[i]);
+        }
+        int average =(int)Average(grades);
+        System.out.println("Promedio: "+ average);
+        System.out.println("Calificación: "+ Letter(average));
+
+    }
+}
